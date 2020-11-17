d313ee31b3a79e4a0164fe2ab0eb1d7c83c2cd4f
Michael Law <1365977+lawmicha@users.noreply.github.com>
2020-11-16 16:50:50 -0800

    Add tracking-ios sample

---
+++ tracking-ios/TrackingSample/TrackingSampleApp.swift
@@ -0,0 +1,29 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+import SwiftUI
+import AWSMobileClient
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        AWSMobileClient.default().initialize { (userState, error) in
+            if let userState = userState {
+                print("UserState: \(userState.rawValue)")
+            } else if let error = error {
+                print("error: \(error.localizedDescription)")
+            }
+        }
+        return true
+    }
+}
+
+@main
+struct TrackingSampleApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
