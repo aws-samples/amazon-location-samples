f83214933cf027c457ed02afd8c249d77ba21116
Seth Fitzsimmons <sethfitz@amazon.com>
2021-10-08 13:59:59 -0700

    Use the correct Credential type for TypeScript

--- amazon-location-helpers/index.d.ts
+++ amazon-location-helpers/index.d.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-import { Credentials } from "@aws-sdk/client-cognito-identity";
+import { Credentials } from "@aws-sdk/types";
 import mapboxgl from "maplibre-gl";
 
 interface Config {
