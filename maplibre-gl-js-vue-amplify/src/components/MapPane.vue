4ff4578e0813e872abe4741a889e7b4ef035b2da
Yasunori Kirimoto <yasunorikirimoto@gmail.com>
2021-12-10 03:46:59 +0900

    :tada: Add maplibre-gl-js-vue-amplify (#80)
    * :tada: Add maplibre-gl-js-vue-amplify
    
    This example demonstrates how to use MapLibre GL JS with Amazon Location Service and AWS Amplify within a Vue.js application.

---
+++ maplibre-gl-js-vue-amplify/src/components/MapPane.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="mapPane">
+        <div id="map"></div>
+    </div>
+</template>
+
+<script>
+import { createMap } from 'maplibre-gl-js-amplify';
+
+export default {
+    name: 'MapPane',
+    data() {
+        return {};
+    },
+    mounted: async function () {
+        this.mapCreate();
+    },
+    methods: {
+        mapCreate: async function () {
+            const map = await createMap({
+                container: 'map',
+                center: [139.7648, 35.6794],
+                zoom: 15,
+                bearing: 64.8,
+                pitch: 60,
+                hash: true,
+            });
+            map.on('load', function () {});
+        },
+    },
+};
+</script>
+
+<style scoped>
+#map {
+    z-index: 0;
+    height: 800px;
+}
+</style>
---
+++ maplibre-gl-js-vue-amplify/src/store/index.js
@@ -0,0 +1,9 @@
+import { createStore } from 'vuex';
+
+export default createStore({
+    state: {},
+    mutations: {},
+    getters: {},
+    actions: {},
+    modules: {},
+});
