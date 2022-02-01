9d912ba14c40101b20f12e02d894bb471a0d2da5
imraymondlee <imleeraymond@gmail.com>
2022-01-31 16:56:58 -0800

    Replace use of create-react-app with only required dependencies

---
+++ react-map-gl-map-styles/webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+    alias: {
+      "mapbox-gl": "maplibre-gl"
+    }
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
