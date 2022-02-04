e21decf30ae797f324c914d01eb10bfc40b7e2ea
Andrea Amorosi <dreamorosi@gmail.com>
2022-02-04 18:21:11 +0000

    chore: moved example to vite & bumped packages

---
+++ leaflet-vue-amplify/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<!-- <script setup>
+import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+ -->
+<script setup>
+import MapPane from '@/components/MapPane.vue';
+</script>
+
+<template>
+  <div class="home">
+    <h2>Amazon Location Service x Amplify Geo</h2>
+    <MapPane></MapPane>
+  </div>
+</template>
