08d64c9a0cc7cd46f580a5c136e8a25080682a82
Andrea Amorosi <dreamorosi@gmail.com>
2022-02-04 18:34:02 +0000

    chore: updated maplibre-gl-js-vue-amplify to vite

--- maplibre-gl-js-vue-amplify/src/router/index.js
+++ maplibre-gl-js-vue-amplify/src/router/index.js
@@ -1,17 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
-
-const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: Home,
-    },
-];
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
-});
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    }
+  ]
+})
 
-export default router;
+export default router
---
+++ maplibre-gl-js-vue-amplify/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import MapPane from '@/components/MapPane.vue';
+</script>
+
+<template>
+    <div class="home">
+        <h2>Amazon Location Service x Amplify Geo</h2>
+        <MapPane></MapPane>
+    </div>
+</template>
