9ca470578f31e0c280a823c0f66cd2b35e4e2809
Andrea Amorosi <dreamorosi@gmail.com>
2022-02-04 18:27:07 +0000

    chore: removed commented code

--- leaflet-vue-amplify/src/views/HomeView.vue
+++ leaflet-vue-amplify/src/views/HomeView.vue
@@ -1,14 +1,3 @@
-<!-- <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
- -->
 <script setup>
 import MapPane from '@/components/MapPane.vue';
 </script>
