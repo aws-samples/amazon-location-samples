132bfb96de166172607ae460408aa518979df492
Andrea Amorosi <dreamorosi@gmail.com>
2022-02-06 16:54:10 +0000

    chore: update to react-maplibre-gl@7.x & remove craco

--- maplibre-gl-js-react/src/index.tsx
+++ maplibre-gl-js-react/src/index.tsx
@@ -5,26 +5,23 @@ import React, { StrictMode, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { createRequestTransformer } from "amazon-location-helpers";
 import { ICredentials } from "@aws-amplify/core";
-import Amplify from "aws-amplify";
-import { Auth } from "aws-amplify";
-import ReactMapGL, { MapRequest, NavigationControl, ViewportProps } from "react-map-gl";
-import "maplibre-gl/dist/maplibre-gl.css";
-
-import "./index.css";
+import Map, { TransformRequestFunction, NavigationControl, ViewState } from "react-map-gl";
+import maplibregl from "maplibre-gl";
+import Amplify, { Auth } from "aws-amplify";
 import amplifyConfig from "./aws-exports";
+import 'maplibre-gl/dist/maplibre-gl.css';
+import "./index.css";
 
 Amplify.configure(amplifyConfig);
-
-type RequestTransformFn = (url?: string | undefined, resourceType?: string | undefined) => MapRequest
 
 // Replace with the name of the map that you created on the Amazon Location Service console: https://console.aws.amazon.com/location/maps/home
 const mapName = "<MAP_NAME>";
 
 const App = () => {
   const [credentials, setCredentials] = useState<ICredentials>();
-  const [transformRequest, setRequestTransformer] = useState<RequestTransformFn>();
+  const [transformRequest, setRequestTransformer] = useState<TransformRequestFunction>();
 
-  const [viewport, setViewport] = React.useState<Partial<ViewportProps>>({
+  const [viewState, setViewState] = React.useState<Partial<ViewState>>({
     longitude: -123.1187,
     latitude: 49.2819,
     zoom: 10,
@@ -48,7 +45,7 @@ const App = () => {
         });
         // wrap the new value in an anonymous function to prevent React from recognizing it as a
         // function and immediately calling it
-        setRequestTransformer(() => tr as unknown as RequestTransformFn);
+        setRequestTransformer(() => tr);
       }
     };
 
@@ -58,18 +55,17 @@ const App = () => {
   return (
     <div>
       {transformRequest ? (
-        <ReactMapGL
-          {...viewport}
-          width="100%"
-          height="100vh"
-          transformRequest={transformRequest}
+        <Map
+          {...viewState}
+          onMove={(e) => {setViewState(e.viewState)}}
+          style={{ width: '100%', height: '100vh' }}
           mapStyle={mapName}
-          onViewportChange={setViewport}
+          transformRequest={transformRequest}
+          mapLib={maplibregl}
+          maxPitch={60}
         >
-          <div style={{ position: "absolute", left: 20, top: 20 }}>
-            <NavigationControl showCompass={false} />
-          </div>
-        </ReactMapGL>
+          <NavigationControl showCompass={false} />
+        </Map>
       ) : (
         <h1>Loading...</h1>
       )}
