02b84b9e41d0094ccb7aa7468e9830ab89ea2cee
Andrea Amorosi <dreamorosi@gmail.com>
2022-02-08 22:03:46 +0000

    chore: updated main deps

--- maplibre-js-react-iot-asset-tracking/src/App.js
+++ maplibre-js-react-iot-asset-tracking/src/App.js
@@ -1,7 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //  SPDX-License-Identifier: MIT-0
 
-import "./App.css";
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import LocationMap from "./LocationMap";
 
