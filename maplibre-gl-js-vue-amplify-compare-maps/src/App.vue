10675658102815709fe8d8a21f6011efbe04661d
inariku <inariku@amazon.co.jp>
2022-07-28 02:54:33 +0900

    add new model syncronizing two maps

--- maplibre-gl-js-vue-amplify-compare-maps/src/App.vue
+++ maplibre-gl-js-vue-amplify-compare-maps/src/App.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
     <el-container>
-      <el-header height="48px" >Amazon Location × Amplify Geo</el-header>
+      <el-header>
+        <el-form-item label="Pitch">
+          <el-input-number v-model="pitch" :min="0" :max="60" :step="5" @change="pitchChange" />
+        </el-form-item>
+      </el-header>
       <el-main>
         <el-row>
-          <el-col :span=12>
-            <leftMap/>
-          </el-col>
-          <el-col :span=12>
-            <rightMap/>
-          </el-col>
+          <el-col :span="12">
+            <Map
+              mapid="leftMap"
+              :availableMaps="availableMaps"
+              @updateState="updateState"
+              :zoom="zoom"
+              :center="center"
+              :pitch="pitch"
+            ></Map
+          ></el-col>
+          <el-col :span="12">
+            <Map
+              mapid="rightMap"
+              :availableMaps="availableMaps"
+              @updateState="updateState"
+              :zoom="zoom"
+              :center="center"
+              :pitch="pitch"
+            ></Map
+          ></el-col>
         </el-row>
       </el-main>
     </el-container>
@@ -17,20 +35,32 @@
 </template>
 
 <script>
-import leftMap from './components/leftMap';
-import rightMap from './components/rightMap';
+import { Geo } from 'aws-amplify';
+import { ref } from 'vue';
+import Map from './MapPane.vue';
 
 export default {
-  data() {
-  
+  components: { Map },
+  setup() {
+    const availableMaps = ref(Geo.getAvailableMaps());
+    const zoom = ref(14);
+    const center = ref([139.7648, 35.6794]);
+    const pitch = ref(30);
+    const updateState = (...args) => {
+      zoom.value = args[0];
+      center.value = args[1];
+    };
+    return {
+      availableMaps,
+      zoom,
+      center,
+      pitch,
+      updateState,
+    };
   },
-  components: {
-    leftMap: leftMap,
-    rightMap: rightMap
-  } 
-}
+};
 </script>
 
 <style>
-@import "@/assets/style.css";
+@import '@/assets/style.css';
 </style>
