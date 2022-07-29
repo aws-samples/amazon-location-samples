17f150e1c9c405f22838d4cd02219ea3bd20a07b
inariku <inariku@amazon.co.jp>
2022-07-29 21:08:11 +0900

    update

--- maplibre-gl-js-vue-amplify-compare-maps/src/main.js
+++ maplibre-gl-js-vue-amplify-compare-maps/src/main.js
@@ -16,6 +16,7 @@ import {
 import '@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css';
 import 'maplibre-gl-js-amplify/dist/public/amplify-geocoder.css';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import 'element-plus/theme-chalk/index.css';
 
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
