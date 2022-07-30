801139705b93d79a2b896c4b815b9bd5f62fa01b
Andrea Amorosi <dreamorosi@gmail.com>
2022-07-30 19:10:27 +0200

    chore: update react-map-gl-map-styles

--- react-map-gl-map-styles/src/configuration.js
+++ react-map-gl-map-styles/src/configuration.js
@@ -1,19 +1,20 @@
 // This configuration file is a single place to provide any values to set up the app
 
-export const IDENTITY_POOL_ID = "XX-XXXX-X:XXXXXXXX-XXXX-1234-abcd-1234567890ab"; // REQUIRED - Amazon Cognito Identity Pool ID
+export const IDENTITY_POOL_ID =
+  "eu-west-1:ccf1eafc-5862-4736-8bf4-450a0f887ccc"; // REQUIRED - Amazon Cognito Identity Pool ID
 
-export const REGION = "XX-XXXX-X"; // REQUIRED - Amazon Cognito Region
+export const REGION = "eu-west-1"; // REQUIRED - Amazon Cognito Region
 
 // These are the different Amazon Location Service map styles that users can switch to display
 export const MAPS = [
   {
-    NAME: "XXXXXXXXXXX", // REQUIRED - Amazon Location Service Map resource name
-    STYLE: "XXXXXXXXXXX", // REQUIRED - String representing the style of map resource
-    LABEL: "XXXXXXXXXXX" // REQUIRED - Text to be displayed for the map in the layer control box
+    NAME: "lightMap", // REQUIRED - Amazon Location Service Map resource name
+    STYLE: "VectorEsriLightGrayCanvas", // REQUIRED - String representing the style of map resource
+    LABEL: "Light", // REQUIRED - Text to be displayed for the map in the layer control box
   },
   {
-    NAME: "XXXXXXXXXXX",  // REQUIRED - Amazon Location Service Map resource name
-    STYLE: "XXXXXXXXXXX", // REQUIRED - String representing the style of map resource
-    LABEL: "XXXXXXXXXXX" // REQUIRED - Text to be displayed for the map in the layer control box
-  }
+    NAME: "darkMap", // REQUIRED - Amazon Location Service Map resource name
+    STYLE: "VectorEsriDarkGrayCanvas", // REQUIRED - String representing the style of map resource
+    LABEL: "Dark", // REQUIRED - Text to be displayed for the map in the layer control box
+  },
 ];
