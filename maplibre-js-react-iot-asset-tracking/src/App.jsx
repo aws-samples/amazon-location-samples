4fe436b567acf71bbe645ad9be6694b4c3cb64d7
Andrea Amorosi <dreamorosi@gmail.com>
2022-07-31 01:43:20 +0200

    chore: updated sample (#179)

---
+++ maplibre-js-react-iot-asset-tracking/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { Geo } from "aws-amplify";
+import { MapView, Text } from "@aws-amplify/ui-react";
+import { NavigationControl } from "react-map-gl";
+import Markers from "./components/Markers";
+import LineOverlay from "./components/LineOverlay";
+import useTracker from "./hooks/useTracker";
+
+function App() {
+  const [trackerPositions] = useTracker({
+    DeviceId: "thing123",
+    TrackerName: "trackerAsset01", // This is the Tracker name, change it according to your own setup
+    EndTimeExclusive: new Date(),
+    StartTimeInclusive: new Date(
+      new Date().getTime() - 1000 * 60 * 60 * 24 * 30
+    ),
+  });
+
+  return (
+    <>
+      <MapView
+        initialViewState={{
+          longitude: -123.1169,
+          latitude: 49.2824,
+          zoom: 16,
+        }}
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <NavigationControl showCompass={false} />
+        <Markers trackerPositions={trackerPositions} />
+        <LineOverlay trackerPositions={trackerPositions} />
+      </MapView>
+    </>
+  );
+}
+
+export default App;
---
+++ maplibre-js-react-iot-asset-tracking/src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
+import "@aws-amplify/ui-react/styles.css";
+import "./index.css";
+
+Amplify.configure(config);
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
