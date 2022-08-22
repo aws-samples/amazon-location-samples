59e5a90d60d32436b95146adf328043eb67b4334
Justin Knoetzke <jfk@amazon.com>
2022-08-22 19:05:34 -0400

    Now shows the track the user took.

--- location-tracking-ios/location-tracking-ios/location-tracking-ios/ContentView.swift
+++ location-tracking-ios/location-tracking-ios/location-tracking-ios/ContentView.swift
@@ -8,26 +8,32 @@
 import SwiftUI
 import AmplifyMapLibreUI
 import CoreLocation
+import Mapbox
 
- 
+
 struct MapView: View {
+    @State var coordinates = [CLLocationCoordinate2D]()
     @StateObject var viewModel = MapViewModel()
     @StateObject private var mapState = AMLMapViewState(
-        zoomLevel: 8,
-        center: CLLocationCoordinate2D(latitude: 39.7392, longitude: -104.9903)
+        zoomLevel: 8
     )
 
     var body: some View {
+        
+
         ZStack(alignment: .top) {
+            
             AMLMapView(mapState: mapState)
                 .showUserLocation(true)
                 .edgesIgnoringSafeArea(.all)
                 .onReceive(mapState.$userLocation, perform: { location in
                 // you're handed the location update here as a CLLocationCoordinate2D through `location`
                     if(location != nil) {
-                        mapState.center = CLLocationCoordinate2D(latitude: location!.latitude, longitude: location!.longitude)
+                         mapState.center = CLLocationCoordinate2D(latitude: location!.latitude, longitude: location!.longitude)
+                         coordinates.append(CLLocationCoordinate2D(latitude: location!.latitude, longitude: location!.longitude))
+                         let polyline = MGLPolyline(coordinates: coordinates, count: UInt(coordinates.count))
+                         mapState.mapView?.addAnnotation(polyline)
                     }
-                    
               }
         )}
     }
