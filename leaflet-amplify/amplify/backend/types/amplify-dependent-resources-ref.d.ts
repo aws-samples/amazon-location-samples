13864dc5b66ab5d15c3013e568f15b06abac79af
Yasunori Kirimoto <yasunorikirimoto@gmail.com>
2022-10-18 23:01:13 +0900

    leaflet-amplify add

---
+++ leaflet-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,21 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "leafletstarter3981405c": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "geo": {
+        "mapcf4954c3": {
+            "Name": "string",
+            "Style": "string",
+            "Region": "string",
+            "Arn": "string"
+        }
+    }
+}
