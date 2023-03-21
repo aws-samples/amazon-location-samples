fa4d96aaacd8313d27aa7b71f82ec8443679d1e8
mepa1363 <pooraziz@amazon.com>
2023-03-21 11:47:28 -0600

    add config file

---
+++ amazon-location-route-planner/src/config.js
@@ -0,0 +1,5 @@
+const identityPoolId = "IDENTITY_POOL_ID";
+const mapName = "TruckDemoMap";
+const routeCalculatorName = "TruckDemoRouteCalculator";
+
+export { identityPoolId, mapName, routeCalculatorName };
