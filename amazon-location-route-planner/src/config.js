07d0d7612b10fe63853ddf8ef7c9e7e0ac1fee32
Andrea Amorosi <dreamorosi@gmail.com>
2023-06-09 13:53:27 +0000

    chore: converted all node samples to monorepo

--- amazon-location-route-planner/src/config.js
+++ amazon-location-route-planner/src/config.js
@@ -1,5 +1,5 @@
-const identityPoolId = "IDENTITY_POOL_ID";
-const mapName = "TruckDemoMap";
-const routeCalculatorName = "TruckDemoRouteCalculator";
+const identityPoolId = "eu-west-1:73b95e8e-bb94-45ef-96a9-7d799d711039";
+const mapName = "map2bf94dcb-dev";
+const routeCalculatorName = "RouteCalculator-map2bf94dcb-dev";
 
 export { identityPoolId, mapName, routeCalculatorName };
--- react-map-gl-amplify-here-map/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ react-map-gl-amplify-here-map/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,26 +1,26 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "awsheredemo4fc859b5": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
-    "geo": {
-        "map2bf94dcb": {
-            "Name": "string",
-            "Style": "string",
-            "Region": "string",
-            "Arn": "string"
-        },
-        "placeindex0e5572f7": {
-            "Name": "string",
-            "Region": "string",
-            "Arn": "string"
-        }
+  "auth": {
+    "awsheredemo4fc859b5": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
     }
+  },
+  "geo": {
+    "map2bf94dcb": {
+      "Arn": "string",
+      "Name": "string",
+      "Region": "string",
+      "Style": "string"
+    },
+    "placeindex0e5572f7": {
+      "Arn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
 }
