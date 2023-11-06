afb302ef6d7c792db06c0f817d58ead60dd1e29e
Riku Inada <inariku@amazon.co.jp>
2023-11-06 17:02:24 +0900

    chore: refactor component to use `script setup`

--- maplibre-gl-js-vue-amplify-compare-maps/src/MapPane.vue
+++ maplibre-gl-js-vue-amplify-compare-maps/src/MapPane.vue
@@ -1,98 +1,83 @@
+<script setup>
+import { createMap, createAmplifyGeocoder } from 'maplibre-gl-js-amplify';
+import { NavigationControl } from 'maplibre-gl';
+import { Geo } from 'aws-amplify';
+import { ref, watchEffect, toRefs } from 'vue';
+
+// Define props and emits for the component
+const props = defineProps({
+  id: String,
+  availableMaps: Array,
+  zoom: Number,
+  center: Object,
+  bearing: Number,
+  pitch: Number,
+  ActiveMap: String,
+});
+
+const emits = defineEmits(['active-map-update', 'state-update']);
+
+// Initialize reactive variables
+const renderedMap = ref(Geo.getDefaultMap().mapName);
+const map = ref(null);
+const { id, zoom, center, pitch, bearing, ActiveMap } = toRefs(props);
+
+// Function to create and set up the map
+const mapCreate = async () => {
+  try {
+    // Create the map instance
+    map.value = await createMap({
+      container: id.value,
+      zoom: zoom.value,
+      center: center.value,
+      pitch: pitch.value,
+      bearing: bearing.value,
+    });
+
+    // Add geocoder and navigation controls to the map
+    map.value.addControl(createAmplifyGeocoder({ flyTo: { maxZoom: 14 } }));
+    map.value.addControl(new NavigationControl({ visualizePitch: true }));
+
+    // Listen for mouseover event and emit 'active-map-update'
+    map.value.on('mouseover', () => {
+      emits('active-map-update', id.value);
+    });
+
+    // Listen for move event and emit 'state-update' if it's the active map
+    map.value.on('move', () => {
+      if (ActiveMap.value == id.value) {
+        emits('state-update', map.value.getZoom(), map.value.getCenter(), map.value.getBearing(), map.value.getPitch());
+      }
+    });
+
+    // Watch for changes in renderedMap and update map style
+    // Also, if it's not the active map, update other properties
+    watchEffect(() => {
+      map.value.setStyle(renderedMap.value);
+      if (ActiveMap.value !== id.value) {
+        map.value.setZoom(zoom.value);
+        map.value.setCenter(center.value);
+        map.value.setBearing(bearing.value);
+        map.value.setPitch(pitch.value);
+      }
+    });
+  } catch (error) {
+    console.error('An error occurred while creating the map:', error);
+  }
+};
+
+// Initialize the map when the component is created
+mapCreate();
+</script>
+
 <template>
   <div class="map-header">
     <div>
-      <label>Map Style</label>
+      <label>Map Style: </label>
       <select v-model="renderedMap">
         <option v-for="map in availableMaps" :key="map.mapName" :label="map.style" :value="map.mapName" />
       </select>
     </div>
   </div>
-  <div :id="id" class="map"></div>
+  <div :id="id" class="map" />
 </template>
-
-<script>
-import { createMap, createAmplifyGeocoder } from 'maplibre-gl-js-amplify';
-import { NavigationControl } from 'maplibre-gl';
-import { Geo } from 'aws-amplify';
-import { ref, watch, toRefs } from 'vue';
-
-export default {
-  props: {
-    id: String,
-    availableMaps: Array,
-    zoom: Number,
-    center: Object,
-    pitch: Number,
-    bearing: Number,
-    ActiveMap: String,
-  },
-  setup(props, context) {
-    const renderedMap = ref(Geo.getDefaultMap().mapName);
-    const map = ref(null);
-    const { id, zoom, center, pitch, bearing, ActiveMap } = toRefs(props);
-
-    const mapCreate = async () => {
-      map.value = await createMap({
-        container: id.value,
-        zoom: zoom.value,
-        center: center.value,
-        pitch: pitch.value,
-        bearing: bearing.value,
-      });
-
-      map.value.addControl(createAmplifyGeocoder());
-      map.value.addControl(new NavigationControl({ visualizePitch: true }));
-
-      map.value.on('mouseover', () => {
-        context.emit('active-map-update', id.value);
-      });
-
-      map.value.on('move', () => {
-        if (ActiveMap.value == id.value) {
-          context.emit(
-            'state-update',
-            map.value.getZoom(),
-            map.value.getCenter(),
-            map.value.getBearing(),
-            map.value.getPitch()
-          );
-        }
-      });
-    };
-
-    mapCreate();
-
-    watch(renderedMap, (renderedMap) => {
-      map.value.setStyle(renderedMap);
-    });
-
-    watch(zoom, (zoom) => {
-      if (ActiveMap.value !== id.value) {
-        map.value.setZoom(zoom);
-      }
-    });
-
-    watch(center, (center) => {
-      if (ActiveMap.value !== id.value) {
-        map.value.setCenter([center.lng, center.lat]);
-      }
-    });
-
-    watch(bearing, (bearing) => {
-      if (ActiveMap.value !== id.value) {
-        map.value.setBearing(bearing);
-      }
-    });
-
-    watch(pitch, (pitch) => {
-      if (ActiveMap.value !== id.value) {
-        map.value.setPitch(pitch);
-      }
-    });
-
-    return {
-      renderedMap,
-    };
-  },
-};
-</script>
